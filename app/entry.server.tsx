6bb4c9b99a805365260dba738c84be7bc9dd20cc
sobrien0294 <sara@gilligancreative.com>
2024-05-03 01:48:11 -0400

    patch 2

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -4,7 +4,11 @@ import isbot from 'isbot';
 import {renderToReadableStream} from 'react-dom/server';
 import {createContentSecurityPolicy} from '@shopify/hydrogen';
 
-
+createContentSecurityPolicy({
+  connectSrc: [
+    'cosmic-assuring-bengal.ngrok-free.app:*',
+  ],
+});
 
 import {getWeaverseCsp} from '~/weaverse/create-weaverse.server';
 
